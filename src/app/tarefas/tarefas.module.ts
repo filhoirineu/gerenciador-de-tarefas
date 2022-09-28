c829439f2b7d77d360f5b2ce95d73e5059ca6af7
Irineu Filho <irineuacfilho@gmail.com>
2022-09-27 21:21:35 -0300

    adicionado o carregamento das tarefas

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TarefaService } from './shared';
 import { ListarTarefasComponent } from './listar';
-
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ListarTarefasComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
-  providers: [ TarefaService ],
+  providers: [TarefaService],
 })
 export class TarefasModule { }
